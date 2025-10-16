89807f5cdd6afe6a99aa2cf5a68a7322d46c3eb6
Usenmfon <usenmfonuko@gmail.com>
2025-10-16 13:05:06 +0100

    fix: corrected typo

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-header("Contnent-Type: application/json");
+header("Content-Type: application/json");
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET");
 header("Access-Control-Allow-Headers: Content-Type");
