841ef16b6ffaa3de3ac6aac679553c662192428c
Usenmfon <usenmfonuko@gmail.com>
2025-10-16 12:08:28 +0100

    fix: curl

--- public/index.php
+++ public/index.php
@@ -1,5 +1,9 @@
 <?php
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 header("Contnent-Type: application/json");
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET");
